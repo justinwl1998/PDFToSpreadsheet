bcff16ee6b3501a331006d66d386515918f7a6b6
Justin Liu <justinwl2097@gmail.com>
2023-07-24 17:32:29 -0500

    added front end and moved another function into helpers.py

--- helpers.py
+++ helpers.py
@@ -1,5 +1,13 @@
+import PyPDF2
+import os
+import xlwt
+from xlwt import Workbook
+
 # Gets the page where the regular monthly charges are listed,
 # assuming the first page has an "at a glance" view
+
+currency_style = xlwt.XFStyle()
+currency_style.num_format_str = "[$$-409]#,##0.00;-[$$-409]#,##0.00"
 
 def getRMCPage(firstPage):
     # Get page number to find regular monthly charges
@@ -28,3 +36,62 @@ def getCharges(pageText):
             except IndexError:
                 continue
     return chargeList
+
+# Scans all PDF files and writes all charges to a spreadsheet, taking the
+# title of the charge and how much was charged.
+def outputToSpreadsheet(directory):
+    if os.path.exists("output.xls"):
+        os.remove("output.xls")
+    wb = Workbook()
+    sheet1 = wb.add_sheet('Sheet 1')
+    colMod = 0
+    rowMod = 4
+    chargeDict = {}
+    rowList = []
+    chargeIndex = 1
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        if os.path.isfile(f):
+            reader = PyPDF2.PdfReader(f)
+            billPage = getRMCPage(reader.pages[0].extract_text().splitlines()) - 1
+            pageAsText = reader.pages[billPage].extract_text().splitlines()
+
+            charges = getCharges(pageAsText)
+
+            # write date or file name in spreadsheet
+            sheet1.write(rowMod, 0, filename[-14:-4])
+            for i in range(len(charges)):
+                tokens = charges[i].split(' ')
+                chargeName = ' '.join(tokens[:-1])
+
+                # charge must have an uppercase letter at beginning and must not end with a period
+                # this may not work for other bills
+                if chargeName[0].islower() or tokens[-1][-1] == '.':
+                    continue
+                
+                if chargeName not in chargeDict:
+                    chargeDict[chargeName] = chargeIndex
+                    sheet1.write(3, chargeDict[chargeName], chargeName)
+                    chargeIndex += 1
+
+                try:
+                    sheet1.write(rowMod, chargeDict[chargeName], float(tokens[-1][1:]), style=currency_style)
+                except:
+                    # if charge with duplicate name is found, try to add new column,
+                    # this is assuming there is at most only one other similar charge
+                    # on the same billing
+                    dupeIndex = 2
+                    if chargeName + "(" + str(dupeIndex) + ")" not in chargeDict:
+                        chargeDict[chargeName + "(" + str(dupeIndex) + ")"] = chargeIndex
+                        sheet1.write(3, chargeDict[chargeName + "(" + str(dupeIndex) + ")"], chargeName + "(" + str(dupeIndex) + ")")
+                        chargeIndex += 1
+                        
+                    if tokens[-1][0] == '-':
+                        sheet1.write(rowMod, chargeDict[chargeName + "(" + str(dupeIndex) + ")"], -1 * float(tokens[-1][2:]), style=currency_style)
+                    else:    
+                        sheet1.write(rowMod, chargeDict[chargeName + "(" + str(dupeIndex) + ")"], float(tokens[-1][1:]), style=currency_style)
+
+
+            rowMod += 1
+                
+    wb.save('output.xls')
--- pdf2spreadsheet.py
+++ pdf2spreadsheet.py
@@ -3,102 +3,44 @@
 #   
 #   Then output them to a spreadsheet.
 
-import PyPDF2
 import os
-import sys
-import xlwt
-from helpers import getRMCPage, getCharges
-from xlwt import Workbook
+from tkinter import filedialog as fd
+from tkinter import messagebox
+from tkinter import *
+from helpers import getRMCPage, getCharges, outputToSpreadsheet
 
-directory = 'billings'
-currency_style = xlwt.XFStyle()
-currency_style.num_format_str = "[$$-409]#,##0.00;-[$$-409]#,##0.00"
+def directorySelectCallBack():
+    folder_selected = fd.askdirectory()
+    entry_text.set(folder_selected)
+    return
 
-# main code starts here
-print("Read individual file (0) or read all files (1)?\n")
-choice = int(input())
-if choice == 0:
-    fileList = []
-    for filename in os.listdir(directory):
-        f = os.path.join(directory, filename)
-        if not os.path.isdir(f):
-            fileList.append(f)
-
-    for i in range(len(fileList)):
-        print(i, ": ", fileList[i])
-    print("Select file to test: ")
-    fileChoice = int(input())
-
-    if fileChoice > len(fileList):
-        print("Invalid selection")
-        sys.exit()
-
-    reader = PyPDF2.PdfReader(fileList[fileChoice])
-    billPage = getRMCPage(reader.pages[0].extract_text().splitlines()) - 1
-    pageAsText = reader.pages[billPage].extract_text().splitlines()
-
-    print(getCharges(pageAsText))
+def scanCallBack():
+    if os.path.isdir(entry_text.get()):
+        outputToSpreadsheet(entry_text.get())
+        messagebox.showinfo("Info", "Charges written to output.xls")
+    else:
+        messagebox.showerror("Error", "Directory does not exist!")
+    return
     
-elif choice == 1:
-    if os.path.exists("output.xls"):
-        os.remove("output.xls")
-    wb = Workbook()
-    sheet1 = wb.add_sheet('Sheet 1')
-    colMod = 0
-    rowMod = 4
-    chargeDict = {}
-    rowList = []
-    chargeIndex = 1
-    for filename in os.listdir(directory):
-        f = os.path.join(directory, filename)
-        if os.path.isfile(f):
-            print("reading: ", filename[-14:-4])
-            reader = PyPDF2.PdfReader(f)
-            billPage = getRMCPage(reader.pages[0].extract_text().splitlines()) - 1
-            pageAsText = reader.pages[billPage].extract_text().splitlines()
+root = Tk()
+root.title('Xfinity billing statement reader')
+root.resizable(False, False)
+root.geometry('300x150')
+L1 = Label(root, text="Statements directory")
+L1.pack( side = LEFT)
+entry_text = StringVar()
+E1 = Entry(root, bd = 5, state=DISABLED, textvariable=entry_text)
+E1.pack( side = LEFT)
+B1 = Button(root, text= "...", command = directorySelectCallBack)
+B1.pack(side = LEFT)
 
-            charges = getCharges(pageAsText)
-
-            # write date in spreadsheet
-            sheet1.write(rowMod, 0, filename[-14:-4])
-            for i in range(len(charges)):
-                tokens = charges[i].split(' ')
-                chargeName = ' '.join(tokens[:-1])
-
-                # charge must have an uppercase letter at beginning and must not end with a period
-                # this may not work for other bills
-                if chargeName[0].islower() or tokens[-1][-1] == '.':
-                    continue
-                
-                if chargeName not in chargeDict:
-                    chargeDict[chargeName] = chargeIndex
-                    sheet1.write(3, chargeDict[chargeName], chargeName)
-                    chargeIndex += 1
-
-                try:
-                    sheet1.write(rowMod, chargeDict[chargeName], float(tokens[-1][1:]), style=currency_style)
-                except:
-                    # if charge with duplicate name is found, try to add new column,
-                    # this is assuming there is at most only one other similar charge
-                    # on the same billing
-                    dupeIndex = 2
-                    if chargeName + "(" + str(dupeIndex) + ")" not in chargeDict:
-                        chargeDict[chargeName + "(" + str(dupeIndex) + ")"] = chargeIndex
-                        sheet1.write(3, chargeDict[chargeName + "(" + str(dupeIndex) + ")"], chargeName + "(" + str(dupeIndex) + ")")
-                        chargeIndex += 1
-                        
-                    if tokens[-1][0] == '-':
-                        sheet1.write(rowMod, chargeDict[chargeName + "(" + str(dupeIndex) + ")"], -1 * float(tokens[-1][2:]), style=currency_style)
-                    else:    
-                        sheet1.write(rowMod, chargeDict[chargeName + "(" + str(dupeIndex) + ")"], float(tokens[-1][1:]), style=currency_style)
+B2 = Button(root, text="Scan into spreadsheet", command = scanCallBack)
+B2.place(relx=0.5, rely=0.8, anchor=CENTER)
+root.mainloop()
+#root.withdraw()
+#folder_selected = filedialog.askdirectory()
 
 
-            rowMod += 1
-            print()
-                
-    wb.save('output.xls')
-else:
-    print("Invalid choice")
 
 
 
